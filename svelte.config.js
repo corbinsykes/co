ec98cf31be67840ba00c9c4a354db23e0701cbe8
Corbin <corbin@corbins-mini.home>
2021-11-10 10:52:28 -0500

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
@@ -7,7 +8,7 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
+	adapter: adapter(),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
