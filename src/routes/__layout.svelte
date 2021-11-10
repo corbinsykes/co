3383140303cd20454752f14fc9b51eff07383f73
Corbin <corbin@corbins-mini.home>
2021-11-10 10:38:52 -0500

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<!-- <script type="module">
+    import '../../static/reset.css';
+</script> -->
+<style lang="scss">
+    @import '../../static/reset.css';
+</style>
+
+<slot></slot>
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		paths: {
+			base: dev ? '' : '/co'
+		}
+	}
+};
+
+export default config;
