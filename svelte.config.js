4b9706e8d06cb22ee6fd408aa2dce958584060ce
Corbin <corbin@corbins-mini.home>
2021-11-10 11:41:36 -0500

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import preprocess from 'svelte-preprocess';
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
@@ -13,9 +13,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		paths: {
-			base: dev ? '' : '/co'
+			base: '/co'
 		}
 	}
 };
-
-export default config;
